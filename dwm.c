938d0543b3a7c663c7feea7dfa24dfadef12a99b
AdmiralRivera <165850545+AdmiralRivera@users.noreply.github.com>
2025-06-25 20:39:49 -0500

    Add files via upload

--- dwm.c
+++ dwm.c
@@ -236,6 +236,7 @@ static int applysizehints(Client *c, int *x, int *y, int *w, int *h,
 static void arrange(Monitor *m);
 static void arrangemon(Monitor *m);
 static void attach(Client *c);
+static void attachBelow(Client *c);
 static void attachstack(Client *c);
 static void buttonpress(XEvent *e);
 static void checkotherwm(void);
@@ -281,7 +282,7 @@ static void manage(Window w, XWindowAttributes *wa);
 static void mappingnotify(XEvent *e);
 static void maprequest(XEvent *e);
 static void monocle(Monitor *m);
-static void motionnotify(XEvent *e);
+static void motionnotify(XEvent *e); 
 static void movemouse(const Arg *arg);
 static void moveorplace(const Arg *arg);
 static Client *nexttiled(Client *c);
@@ -597,6 +598,22 @@ void attach(Client *c) {
     c->next = c->mon->clients;
     c->mon->clients = c;
   }
+}
+
+void
+attachBelow(Client *c)
+{
+  //If there is nothing on the monitor or the selected client is floating attach as normal
+  if(c->mon->sel == NULL || c->mon->sel == c || c->mon->sel->isfloating) {
+	  attach(c);
+	  return;
+  }
+
+  //Set the new client's next property to the same as the currently selected clients next
+  c->next = c->mon->sel->next;
+  //set the currently selected clients next property to the new client
+  c->mon->sel->next = c;
+
 }
 
 void attachstack(Client *c) {
@@ -2095,7 +2112,10 @@ void manage(Window w, XWindowAttributes *wa) {
 	  c->isfloating = c->oldstate = trans != None || c->isfixed;
   if (c->isfloating)
     XRaiseWindow(dpy, c->win);
-  attach(c);
+  if( attachbelow )
+	  attachBelow(c);
+  else
+	  attach(c);
   attachstack(c);
   XChangeProperty(dpy, root, netatom[NetClientList], XA_WINDOW, 32,
                   PropModeAppend, (unsigned char *)&(c->win), 1);
@@ -2700,7 +2720,10 @@ void sendmon(Client *c, Monitor *m) {
   detachstack(c);
   c->mon = m;
   c->tags = m->tagset[m->seltags]; /* assign tags of target monitor */
-  attach(c);
+  if( attachbelow )
+	  attachBelow(c);
+  else
+	  attach(c);
   attachstack(c);
   setclienttagprop(c);
   focus(NULL);
@@ -3456,7 +3479,10 @@ int updategeom(void) {
 		    m->clients = c->next;
 		    detachstack(c);
 		    c->mon = mons;
-		    attach(c);
+		    if( attachbelow )
+			    attachBelow(c);
+		    else
+			    attach(c);
 		    attachstack(c);
 	    }
 	    if (m == selmon)
